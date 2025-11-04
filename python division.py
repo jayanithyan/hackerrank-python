abcff091546058ab97860d6873dfaed3c05dee2f
jayanithyan <srisarankamalyarns83@gmail.com>
2025-11-04 13:17:43 +0530

    python loops

--- python division.py
+++ python division.py
@@ -4,7 +4,7 @@ The provided code stub reads two integers,  and , from STDIN.
 
 Add logic to print two lines. The first line should contain the result of integer division,  // . The second line should contain the result of float division,  / .
 
-No rounding or formatting is necessary.."""
+No rounding or formatting is necessary..."""
 
 if __name__ == '__main__':
     a = int(input())
