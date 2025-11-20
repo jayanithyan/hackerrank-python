eeb1777b20843b1b71d214ad97f2c2549dfc63fc
jayanithyan <srisarankamalyarns83@gmail.com>
2025-11-20 12:39:27 +0530

    349. Intersection of Two Arrays

---
+++ arrays and hashing/349. Intersection of Two Arrays.py
@@ -0,0 +1,16 @@
+"""Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order."""
+
+
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        p=[]
+        for i in nums1:
+            if i in nums2:
+                if i not in p:
+                    p.append(i)
+        return p
