74027401f2e1b6b7420bc482c7182bebe5f8a1c6
jayanithyan <srisarankamalyarns83@gmail.com>
2025-11-25 20:39:47 +0530

    136. Single Number

---
+++ arrays and hashing/136. Single Number.py
@@ -0,0 +1,14 @@
+"""Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space."""
+
+
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        for i in nums:
+            if nums.count(i)!=2:
+                return i
